48f56935358b58064cc7b5368fdc874a63ce3008
Neoself <neoself1105@gmail.com>
2023-04-22 10:51:44 +0900

    server.ts landingpage

--- dist/server.js
+++ dist/server.js
@@ -2,11 +2,20 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var apollo_server_1 = require("apollo-server");
 var schema_1 = require("./schema");
+var apollo_server_core_1 = require("apollo-server-core");
 var server = new apollo_server_1.ApolloServer({
     cache: "bounded",
     typeDefs: schema_1.typeDefs,
     resolvers: schema_1.resolvers,
-    introspection: true
+    plugins: [
+        // Install a landing page plugin based on NODE_ENV
+        process.env.NODE_ENV === "production"
+            ? (0, apollo_server_core_1.ApolloServerPluginLandingPageProductionDefault)({
+                graphRef: "my-graph-id@my-graph-variant",
+                footer: false,
+            })
+            : (0, apollo_server_core_1.ApolloServerPluginLandingPageLocalDefault)({ footer: false }),
+    ],
 });
 server
     .listen(process.env.PORT || 4000)
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,23 @@
 import { ApolloServer, gql } from 'apollo-server';
 import { resolvers, typeDefs } from './schema';
-
+import {
+  ApolloServerPluginLandingPageLocalDefault,
+  ApolloServerPluginLandingPageProductionDefault
+} from "apollo-server-core";
 
 const server = new ApolloServer({
   cache: "bounded",
   typeDefs,
   resolvers,
-  introspection:true
+  plugins: [
+    // Install a landing page plugin based on NODE_ENV
+    process.env.NODE_ENV === "production"
+      ? ApolloServerPluginLandingPageProductionDefault({
+          graphRef: "my-graph-id@my-graph-variant",
+          footer: false,
+        })
+      : ApolloServerPluginLandingPageLocalDefault({ footer: false }),
+  ],
 });
 
 server
